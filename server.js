5ad9eda63a3dc517a9fd047ef9b427d0bcd557c6
renzo-c <rcnavarrop@gmail.com>
2018-10-08 01:17:29 -0500

    heroku settings - database test_02

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const Client = new Client({
   ssl: true
 });
 
-express()
-.get('/db', async (req, res) => {
+
+app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table');
+    const result = await client.query('SELECT * FROM employee');
     const results = { 'results': (result) ? result.rows : null};
     res.render('pages/db', results );
     client.release();
