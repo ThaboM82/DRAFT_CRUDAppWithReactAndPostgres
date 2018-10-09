b9be3810bf119c097002ac6134dae250ef778a8d
renzo-c <rcnavarrop@gmail.com>
2018-10-09 18:26:03 -0500

    customizing server.js for heroku

--- components/employeeForm.js
+++ components/employeeForm.js
@@ -75,7 +75,7 @@ export default class employeeNew extends Component {
   }
 
   render() {
-    console.console.log(this.state);
+    console.log(this.state);
     return (
       <div id="main-frame" className="employee-form">
         <form onSubmit={this.handleSubmit.bind(this)}>
