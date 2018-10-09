e44faa6f2b44b3ab83c9278ab81f6abb67f31a25
renzo-c <rcnavarrop@gmail.com>
2018-10-08 20:06:13 -0500

    customizing server.js for heroku

--- server.js
+++ server.js
@@ -22,16 +22,17 @@ app
   // .set('view engine', 'ejs')
   // .get('/', (req, res) => res.render('pages/index'))
   .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query("SELECT * FROM employee");
-      const results = {'results': (result) ? result.rows : null};
-      res.render('pages/db', results);
-      client.release();
-    } catch (err) {
-      console.error("Error_c! : ", err);
-      res.send("Error_b!!! :" + err);
-    }
+    res.send(JSON.stringify({ Hello: ‘World’}));
+    // try {
+    //   const client = await pool.connect()
+    //   const result = await client.query("SELECT * FROM employee");
+    //   const results = {'results': (result) ? result.rows : null};
+    //   res.render('pages/db', results);
+    //   client.release();
+    // } catch (err) {
+    //   console.error("Error_c! : ", err);
+    //   res.send("Error_b!!! :" + err);
+    // }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
