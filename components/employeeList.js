924565c01e637c103aaaa10335a30069164b87ab
renzo-c <rcnavarrop@gmail.com>
2018-10-09 19:05:11 -0500

    customizing server.js for heroku

--- components/employeeList.js
+++ components/employeeList.js
@@ -37,7 +37,8 @@ export default class employeeList extends Component {
 
   render() {
     // console.log("this.state.employees", this.state.employees)
-    return (this.state.employees.length == 0 || this.state.employees == undefined)
+    // return (this.state.employees.length == 0 || this.state.employees == undefined)
+    return (this.state.employees == undefined)
     ? (<p>No employee entries</p>)
     : (
           <div id="main-frame">
